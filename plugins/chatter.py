581e65035e4ca951d1b7355baa6fffcad74ee61a
Ramon Klass <ramon.klass@ims.uni-stuttgart.de>
2012-05-05 21:52:53 +0200

    random chatter fixed

--- plugins/chatter.py
+++ plugins/chatter.py
@@ -1,22 +1,19 @@
 from bot import BotPlugin
+import copy
 
 chatter = BotPlugin()
 
-messages = {}
-messages['fool'] = 'zu Recht.'
-messages['#uasy'] = 'YEAH!'
-messages['feuer'] = "Getadelt wird wer Schmerzen kennt | " \
-    "Vom Feuer das die Haut verbrennt | Ich werf ein Licht " \
-    "| In mein Gesicht | Ein heisser Schrei | Feuer Frei! "
-messages['sonne'] = "Hier kommt die Sonne."
-messages['kaffee'] = "Da bin ich dabei!"
+@chatter.any
+def say_stuff(bot, user, channel, msg):
+    messages = {}
+    messages['fool'] = 'zu Recht.'
+    messages['#uasy'] = 'YEAH!'
+    messages['feuer'] = "Getadelt wird wer Schmerzen kennt | " \
+        "Vom Feuer das die Haut verbrennt | Ich werf ein Licht " \
+        "| In mein Gesicht | Ein heisser Schrei | Feuer Frei! "
+    messages['sonne'] = "Hier kommt die Sonne."
+    messages['kaffee'] = "Da bin ich dabei!"
 
-for m in messages.keys():
-    reply = messages.get(m)
-
-    def tmp(bot, user, channel, msg):
-        bot.msg(channel, reply)
-    tmp.__name__ = "say_%s"%m
-    chatter.add_contains(m, tmp)
-
-chatter.add_contains('blub', lambda bot, user, channel, msg: bot.msg(channel, 'blah'))
+    for m in messages.keys():
+        if m in msg.lower():
+            bot.msg(channel, messages.get(m))
