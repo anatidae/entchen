d3e4b2863deb06c790fd69720a98c28db98f24ff
Andreas Madsack <andreas.madsack@ims.uni-stuttgart.de>
2012-06-28 17:27:08 +0200

    added <nick> option for !nalarm. some code beautification needed

--- plugins/alarm.py
+++ plugins/alarm.py
@@ -7,8 +7,12 @@ alarm = BotPlugin()
 @alarm.command('nalarm')
 def alarmcmd(bot, user, channel, msg):
 
+    # TODOs:
+    # - save all reminders in the factory for a !showalarms command
+    # - maybe: add !delalarm <alarmid>
+
     def showhelp():
-        bot.msg(channel, "<time or minutes> <message>")
+        bot.msg(channel, "[<nick>] <time or minutes> <message>")
 
     def startmsg(username, seconds):
         minutes = math.floor(seconds / 60)
@@ -23,26 +27,42 @@ def alarmcmd(bot, user, channel, msg):
         m = "%s: --- %s ---" % (username, s)
         bot.msg(channel, m)
 
+    username = user.split('!')[0]
+    message = None
+    seconds = None
+
     sp = msg.split()
     if len(sp) < 2:
         showhelp()
         return
 
-    if str(sp[0]).isdigit():
-        seconds = int(sp[0]) * 60
-    else:
-        if ':' not in sp[0]:
-            showhelp()
-            return
-        now = datetime.datetime.now()
-        tm = [int(i) for i in sp[0].split(':')]
-        x = datetime.datetime(now.year, now.month, now.day,
-                              tm[0], tm[1])
-        # calculate datetimedelta and save difference in seconds
-        seconds = (x-now).seconds
+    timevalue = sp[0]
 
-    message = " ".join(sp[1:])
-    username = user.split('!')[0]
+    if str(timevalue).isdigit():
+        seconds = int(timevalue) * 60
+    else:
+        if ':' not in timevalue:
+            timevalue = sp[1]
+            if ':' in timevalue or str(timevalue).isdigit():
+                if str(timevalue).isdigit():
+                    seconds = int(timevalue) * 60
+                # <user> <time> <message>
+                username = sp[0]
+                timevalue = sp[1]
+                message = " ".join(sp[2:])
+            else:
+                showhelp()
+                return
+        if not seconds:
+            now = datetime.datetime.now()
+            tm = [int(i) for i in timevalue.split(':')]
+            x = datetime.datetime(now.year, now.month, now.day,
+                                  tm[0], tm[1])
+            # calculate datetimedelta and save difference in seconds
+            seconds = (x-now).seconds
+
+    if not message:
+        message = " ".join(sp[1:])
 
     bot.factory.reactor.callLater(seconds, f, username, message)
     startmsg(username, seconds)
