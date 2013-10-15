d99148320c0452587d73851cfdf71c48c1b47459
Andreas Madsack <andreas@madflex.de>
2013-10-15 15:20:57 +0200

    added birthday plugin

--- plugins/birthday.py
+++ plugins/birthday.py
@@ -1,8 +1,9 @@
 from bot import BotPlugin
 
-sysinfo = BotPlugin()
+birthday = BotPlugin()
 
-@sysinfo.command('birthday')
-def birthday(bot, user, channel, msg):
+
+@birthday.command('birthday')
+def birthdaycmd(bot, user, channel, msg):
     bot.msg(channel, 'kindchen singt mit allen im Chor: '
             '"Happy Birthday dear %s, Happy Birthday to you!"' % msg)
