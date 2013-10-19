8c59b5d58e5375c2a4b245133635801a928ae280
Andreas Madsack <andreas@madflex.de>
2013-10-19 15:12:28 +0200

    added some test for dice parser

---
+++ plugins/dice/test_dice.py
@@ -0,0 +1,34 @@
+import pytest
+from dice import roll
+import re
+
+
+class TestDice:
+
+    def test_simple(self):
+        x = roll('d6')
+        r = re.findall(r'\( [1-6] \) = [1-6]', x)
+        assert len(r) == 1
+
+    def test_two(self):
+        x = roll('2d6')
+        r = re.findall(r'\( [1-6] \+ [1-6] \) = \d+', x)
+        assert len(r) == 1
+
+    def test_result(self):
+        x = roll('2d6')
+        r = re.findall(r'\( ([1-6]) \+ ([1-6]) \) = (\d+)', x)
+        assert len(r) == 1
+        assert int(r[0][0]) + int(r[0][1]) == int(r[0][-1])
+
+    def test_bigger(self):
+        x = roll('2w1000')
+        r = re.findall(r'\( (\d+) \+ (\d+) \) = (\d+)', x)
+        assert len(r) == 1
+        assert int(r[0][0]) + int(r[0][1]) == int(r[0][-1])
+
+    def test_addition(self):
+        x = roll('2w20+5')
+        r = re.findall(r'\( (\d+) \+ (\d+) \) \+ (5) = (\d+)', x)
+        assert len(r) == 1
+        assert int(r[0][0]) + int(r[0][1]) + int(r[0][2]) == int(r[0][-1])
