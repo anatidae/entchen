a88cc28d63ff32f174b4c82a696e6aa8df76ca55
Andreas Madsack <andreas@madflex.de>
2013-10-19 15:12:01 +0200

    added command wuerfel (German for dice)

--- plugins/dice/__init__.py
+++ plugins/dice/__init__.py
@@ -5,10 +5,12 @@ import random
 
 dice = BotPlugin()
 
+
 @dice.init
 def init():
     random.seed()
 
-@dice.command(['roll', 'dice'])
+
+@dice.command(['roll', 'dice', 'wuerfel'])
 def do_roll(bot, user, channel, msg):
     bot.msg(channel, roll(msg))
