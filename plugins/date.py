e25da754fbac9f705a348c96a9105f20743af127
Arne Schauf <arne@as-webservices.de>
2013-12-11 17:18:53 +0100

    add time and datetime commands to date plugin

--- plugins/date.py
+++ plugins/date.py
@@ -6,5 +6,20 @@ date = BotPlugin()
 
 @date.command('date')
 def say_date(bot, user, channel, msg):
-    m = "Date: %s" % time.strftime("%a, %b %d, %Y", time.localtime())
+    m = "%s" % time.strftime("%a, %b %d, %Y", time.localtime())
+    bot.msg(channel, m)
+
+
+@date.command('time')
+def say_time(bot, user, channel, msg):
+    m = "%s" % time.strftime("%I:%M %p", time.localtime())
+    bot.msg(channel, m)
+
+
+@date.command('dt')
+def say_datetime(bot, user, channel, msg):
+    m = "Todas is %s and it's %s" % (
+        time.strftime("%a, %b %d, %Y", time.localtime()),
+        time.strftime("%I:%M %p", time.localtime())
+    )
     bot.msg(channel, m)
