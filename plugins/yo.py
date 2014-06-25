1377dc1f67d0453899bbdd62354b5dce81ff46f8
Andreas Madsack <andreas@madflex.de>
2014-06-25 13:05:25 +0200

    improved yo, added noe

--- plugins/yo.py
+++ plugins/yo.py
@@ -12,7 +12,24 @@ def show_yo(bot, user, channel, msg):
     m = "%s sends you: Yo!" % username
     sp = msg.split()
     try:
-        to_user = str(sp[0])
-        bot.msg(to_user, m)
+        for elem in sp:
+            to_user = str(elem)
+            bot.msg(to_user, m)
     except:
         bot.msg(channel, "!yo <nick>")
+
+
+@yo.command('noe')
+def show_noe(bot, user, channel, msg):
+    """
+        Send noe to someone
+    """
+    username = user.split('!')[0]
+    m = "%s sends you: Noe!" % username
+    sp = msg.split()
+    try:
+        for elem in sp:
+            to_user = str(elem)
+            bot.msg(to_user, m)
+    except:
+        bot.msg(channel, "!noe <nick>")
